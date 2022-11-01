816276139c4fc6dab4d922213cd166f5f577e5e0
Tibbo93 <alessandrotibaldi93@gmail.com>
2022-11-01 19:06:32 +0100

    Add test suite

---
+++ src/test/java/com/github/joinabletablesearch/TestJoinableTableSearch.java
@@ -0,0 +1,108 @@
+package com.github.joinabletablesearch;
+
+import com.github.joinabletablesearch.deserializer.TableDeserializer;
+import com.github.joinabletablesearch.model.Table;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestJoinableTableSearch {
+
+    @Test
+    public void testIndexingAndExportDataset() {
+        System.out.println("START - Test indexing and export dataset");
+        System.out.println("===============================================================================================\n");
+
+        long start = System.currentTimeMillis();
+        Path indexPath = Paths.get("lucene-index");
+
+        try {
+            Indexer indexer = new Indexer(indexPath);
+            indexer.index("tables.json");
+            indexer.getDataset().exportDataset();
+            System.out.println(indexer.getDataset());
+
+            System.out.println("\n===============================================================================================");
+            System.out.println("END - Execution time: " + (System.currentTimeMillis() - start) / 1000 + " seconds");
+        } catch (IOException e) {
+            System.out.println("Error during indexing json: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSearchLargeTable() {
+        System.out.println("START - Test search with large table");
+
+        Path indexPath = Paths.get("lucene-index");
+        Searcher searcher = new Searcher(indexPath);
+
+        try {
+            Table table = this.getTable("largeTable.json");
+            long start = System.currentTimeMillis();
+            System.out.println("Start searching...");
+            List<Integer> topKSet = searcher.search(table.getColumns().get(0), 2, table.getMongoId().getOid());
+            long time = (System.currentTimeMillis() - start) / 1000;
+            System.out.println("END - Execution time: " + time + " seconds");
+
+            System.out.println("\n===============================================================================================");
+            System.out.println("RESULT - k sets with highest counts: " + topKSet);
+        } catch (IOException e) {
+            System.out.println("Cannot open table json. Error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSearchMultiTable() {
+        System.out.println("START - Test search with large table");
+
+        Path indexPath = Paths.get("lucene-index");
+        Searcher searcher = new Searcher(indexPath);
+
+        List<Long> tempi = new ArrayList<>();
+        int[] tables = {4, 12, 25, 50, 100, 200, 401, 802, 1599, 3143, 6102};
+        //int[] tables = {4,12,25,50,100};
+        for (int n : tables) {
+            try {
+                Table table = this.getTable("tables/table-" + n + ".json");
+                long start = System.currentTimeMillis();
+                System.out.println("Start searching...");
+                List<Integer> topKSet = searcher.search(table.getColumns().get(0), 2, table.getMongoId().getOid());
+                long time = (System.currentTimeMillis() - start) / 1000;
+                tempi.add(time);
+                System.out.println("END - Execution time: " + time + " seconds");
+
+                System.out.println("\n===============================================================================================");
+                System.out.println("RESULT - k sets with highest counts: " + topKSet);
+            } catch (IOException e) {
+                System.out.println("Cannot open table json. Error: " + e.getMessage());
+            }
+        }
+        System.out.println("Tempi: " + tempi);
+        System.out.println("Execution time map: " + searcher.getExecutionTimeList());
+    }
+
+    private Table getTable(String tablePath) throws IOException {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Table.class, new TableDeserializer())
+                .create();
+
+        try (JsonReader jsonReader = new JsonReader(
+                new InputStreamReader(
+                        new FileInputStream(tablePath), StandardCharsets.UTF_8))) {
+
+            jsonReader.setLenient(true);
+
+            return gson.fromJson(jsonReader, Table.class);
+        }
+    }
+}
